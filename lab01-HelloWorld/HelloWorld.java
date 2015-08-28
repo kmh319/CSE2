3d7c4d4da6d56526dcfe2ade401c713f76e7647e
kmh319 <kmh319@lehigh.edu>
2015-08-28 13:19:38 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+////
+//Kaitlyn Hennessy
+//kmh319
+//
+//defining the class
+public class HelloWorld{
+    //main method
+    public static void main(String[] args){
+        //Message
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("#                         #");
+        System.out.println("#    Hello World 2015!    #");
+        System.out.println("#                         #");
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("testing");
+    }
+}
