186bf6b9956e20d805a02e362f72f2590589949d
kmh319 <kmh319@lehigh.edu>
2015-08-28 17:55:20 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//
+//Kaitlyn Hennessy
+//hw01-WelcomeClass
+//
+public class WelcomeClass{
+    public static void main(String[] args){
+        //Welcome box
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        //End welcome box
+        //Begin name diamond
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-K--M--H--3--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        //End name diamond
+        //Tweet-length autobiographical statement.
+        System.out.println("My family and I went to Universal Studios this summer.");
+        System.out.println("I also went to Magic Kingdom for a day with my mom.");
+        
+    }
+}
+        
