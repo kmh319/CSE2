bd3148767b877860d4babf6d449a35d6311edb1b
kmh319 <kmh319@lehigh.edu>
2015-10-30 13:18:04 +0000

    updated Arrays

--- Test.java
+++ Test.java
@@ -1,19 +1,42 @@
-
+import java.util.Scanner;
 public class Test {
-      public static int addAll() {
-    int i =1;
-    int j = 0;
-    while(i <10){
-     j = j +i;
-     if(i == 5){
-       return j;
+     public static String multiply(int a, int b){
+         String returnVal="";
+         int runs=0;
+         int total=0;
+         while(a<=b){
+             if(runs==0){
+             returnVal+=a;
+             runs++;
+             a++;
+             total+=a;
+             }
+             else{
+                 returnVal+="*"+a;
+                 a++;
+                 total*=a;
+             }
+             
+             
+         }
+         returnVal+="="+total;;
+         return returnVal;
      }
-     i++;
-    }
-    return i + j;
-  }
-  public static void main(String args[]) {
-    System.out.println(addAll());
-  }
-
+     public static void main (String[] args){
+         Scanner scan=new Scanner(System.in);
+         System.out.println("Enter your first integer(must be greater than or equal to zero): ");
+         int x=scan.nextInt();
+         while(x<0){
+             System.out.println("Invalid entry.\nEnter your first integer(must be greater than or equal to zero): ");
+             x=scan.nextInt();
+         }
+         System.out.println("Enter your second integer(must be greater than or equal to zero): ");
+         int y=scan.nextInt();
+         while(y<0){
+             System.out.println("Invalid entry.\nEnter your second integer(must be greater than or equal to zero): ");
+             y=scan.nextInt();
+         }
+         System.out.println(multiply(x,y));
+         
+     }
 }
---
+++ lab10/Arrays.java
@@ -0,0 +1,47 @@
+//A method to generate a user-determined number of values and perform various computations.
+//
+//@author Kaitlyn Hennessy
+//@version 10/30/15
+
+import java.util.Scanner;//Import Scanner class.
+public class Arrays{
+    public static void main (String[] args){//Create main method
+        constructArray();//Call constructArray();
+    }
+    //Method to set the array's number of values.
+    public static void constructArray(){
+        Scanner scan=new Scanner(System.in);//Create Scanner object.
+        System.out.println("How many numbers will you generate?");//Prompt user for int.
+        int arraySize=scan.nextInt();//Save user entered int.
+        while(arraySize<=0){//Check validity of int.
+            System.out.println("You must generate more than 0 numbers.\nHow many numbers will you generate?");//Inform user the entry was invalid, ask for another.
+            arraySize=scan.nextInt();//Save new user entered int.
+        }
+        fillArray(arraySize);//Call fillArray()
+    }
+    //Method to give each position of the array a random value.
+    public static void fillArray(int aSize){
+        int[] randomArray=new int [aSize];//Declare the array.
+        for(int x=0;x<randomArray.length;x++){//Loop iterates through each position.
+            randomArray[x]=(int)(Math.random()*100);//Generate a random number from 0-100, put it in randomArray[x].
+            System.out.println("Number "+(x+1)+": "+randomArray[x]);//Tell the user the value.
+        }
+        doMath(randomArray);//Call doMath()
+    }
+    //Method to compute the sum and average of the array and compare the values against the average.
+    public static void doMath(int[]randomArray){
+        int sum=0;//Initialize the variable that will hold the sum.
+        int avg=0;//Initialize the variable that will hold the average.
+        for(int y=0;y<randomArray.length;y++){//Loop through the array.
+            sum+=randomArray[y];//Increment the sum by the value in randomArray[y].
+        }
+        avg=sum/(randomArray.length);//Generate the average by dividing the sum by the array's length.
+        System.out.println("Sum: "+sum+"\nAverage: "+avg);//Tell the user the sum and average.
+        System.out.println("Values greater than or equal to "+avg+":");
+        for(int z=0;z<randomArray.length;z++){//Loop through the array positions.
+            if(randomArray[z]>=avg){//Check if value at randomArray[z] for if it is >=avg.
+                System.out.print(randomArray[z]+"\t");//If it is, print the average.
+            }
+        }
+    }
+}
